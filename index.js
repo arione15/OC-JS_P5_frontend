c926c1f1f87ccb8c824086a2ed2c73abfdc75b05
arione15 <massinoueslati@gmail.com>
2020-12-27 22:54:24 +0100

    P1,2,3 fonctionnelles, P4 non fonctionnelle

--- index.js
+++ index.js
@@ -1,65 +1,25 @@
-
 fetch(`http://localhost:3000/api/cameras`)
 	.then(resp => {
 		console.log(resp)
 		return resp.json()
 	})
+	/* qu'on peut écrire aussi .then(resp => resp.json()) */
 	.then(respJson => {
 		console.log(respJson)
 		let output = ''
 		const elementParent = document.querySelector('#wrapper')
 		respJson.forEach(camera => {
+			console.log('typeof camera: ' + typeof camera);
 
-			
-
-			// let elementLien=document.createElement('a')
-			// elementLien.setAttribute('href', `details.html?id=${camera._id}`)
-
-			// let elementImage=document.createElement('img')
-			// elementImage.setAttribute('src', camera.imageUrl)
-			// elementImage.setAttribute('alt', '#')
-			// elementImage.classList.add('card-img-top', 'rounded-circle')
-
-			// let elementCard=document.createElement('div')
-			// elementCard.classList.add('card-body')
-
-			// let elementNom=document.createElement('h5')
-			// elementNom.classList.add('card-title')
-			// elementNom.innerHTML=`Nom : ${camera.title}`
-
-			// let elementPrix=document.createElement('h6')
-			// elementPrix.classList.add('card-title')
-			// elementPrix.innerHTML=`${camera.price/100}  €`
-
-			// let elementDescription=document.createElement('p')
-			// elementDescription.classList.add('card-text')
-			// elementDescription.innerHTML=`${camera.description}`
-
-
-
-
-			// elementParent.appendChild(elementLien)
-			// elementLien.appendChild(elementImage)
-
-			// elementParent.appendChild(elementCard)
-			// elementCard.appendChild(elementNom)
-			// elementCard.appendChild(elementPrix)
-			// elementCard.appendChild(elementDescription)
-
-
-
-			output += `<a href="details.html?id=${camera._id}"><img src="${camera.imageUrl}" alt="#"
-								class="card-img-top rounded-circle"></a>
+			output += `<td class="card col-8 col-md-5 col-lg-3 m-3"><a href="details.html?id=${camera._id}"><img src="${camera.imageUrl}" alt="#"
+								class="card-img-top rounded-pill p-1"></a>
 						<div class="card-body">
 							<h5 class="card-title">Nom : ${camera.name}</h5>
 							<h6 class="card-title">Prix : ${camera.price/100} €</h6>
-								<p class="card-text">${camera.description}</p>
-						</div>`
-			// l'objet "camera" contient la variable "name" et non pas "title", il faut
-			// donc utiliser "camera.name" au lieu de "camera.title"
-			// <h5 class="card-title">Nom : ${camera.title}</h5>
+							<h6 class="card-title">Lentille(s) disponible(s) : ${camera.lenses.length}</h6>
+								<p class="card-text pIndex">${camera.description}</p>
+						</div></td>`
 			elementParent.innerHTML = output
-
 		})
-
 	})
+
--- panier.js
+++ panier.js
@@ -1,10 +1,11 @@
 let panier = [] //contient les cameras ajoutées au panier
-let products = [] //contient les identifiants des cameras à commander
+let products = [] //contient les couples identifiant/lentille des cameras à commander. Un tableau d'objets ???
 let total = 0
 
-for (let i = 0; i < localStorage.length; i++) { //parcourir tous les identifiants stockés dans le navigateur
-    let id = localStorage.key(i); //récupérer les identifiants stockés dans le navigateur
-
+/** Parcourir tous les couples identifiant/lentille stockés dans le navigateur */
+for (let i = 0; i < localStorage.length; i++) { 
+    let id = JSON.stringify(localStorage.key(i)) /* Récupère les couples identifiant/lentille */
+console.log(JSOPN.parse(id))
         fetch(`http://localhost:3000/api/cameras/${id}`)
             .then(resp => {
                 console.log(resp)
@@ -16,6 +17,7 @@ for (let i = 0; i < localStorage.length; i++) { //parcourir tous les identifiant
                 console.log(panier);
 
                 products.push(panier[i]._id) //stocker les identifiants des cameras à commander dans la liste "products"
+                /** Comment stocker le couple id/lense ???? */
                 console.log(products);
 
                 total = total + panier[i].price //calculer le total
@@ -23,18 +25,28 @@ for (let i = 0; i < localStorage.length; i++) { //parcourir tous les identifiant
                 let output = ''
 				const elementParent = document.querySelector('#wrapper')
 				const elementTotal = document.querySelector('#total')
+				const elementLentille = document.querySelector('#lentilleUnitaire')
+				const elementPrixUnitaire = document.querySelector('#prixUnitaire')
 
                 for (let j = 0; j < panier.length; j++) {
-                    output += `<a href="details.html?id=${panier[j]._id}"><img src="${panier[j].imageUrl}" alt="#"
-                                class="card-img-top">
-                            </a>
-                            <div class="card-body">
-                                <h5 class="card-title">Nom : ${panier[j].name}</h5>
+                    output += `<a href="details.html?id=${panier[j]._id}"><img src="${panier[j].imageUrl}" alt="#" class="card-img-top">
+                            </a>`
+							elementLentille.innerHTML = panier[j].lenses
+							elementPrixUnitaire.innerHTML = panier[j].price/100 + " €"
+
+							/*
+							<div class="card-body">
+								<h5 class="card-title">Nom : ${panier[j].name}</h5>
+								<h7 class="card-title">Lentille : ${panier[j].lenses}</h7>
                                 <h6 class="card-title">Prix : ${panier[j].price/100} €</h6>
-                            </div>`
+							</div>
+							*/
+								/*divCard.innerHTML = output
+							elementParent.innerHTML = divCard*/
                 }
                 elementParent.innerHTML = output
-                elementTotal.innerHTML = "Total: " + total/100 + " €"
+				elementTotal.innerHTML = total/100 + " €"
+				
             })
 }
 
@@ -68,7 +80,7 @@ function Confirmer(e) {
             "Accept": 'application/json, text/plain, "/"',
             "Content-type": "application/json"
         },
-        body: JSON.stringify({ contact, products }) //JSON.stringify pour convertir JSON en string
+        body: JSON.stringify({ contact, products })
     })
         .then(response => response.json())
         .then(json => {
