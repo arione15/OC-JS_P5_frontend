e391d097d9dd1e048caee9442b708267e563e4f8
arione15 <massinoueslati@gmail.com>
2020-12-27 22:54:24 +0100

    P1 et P2 fonctionnelles, P3 et P4 non

--- details.js
+++ details.js
@@ -1,46 +1,99 @@
-//dans la page "details.html", il faut obtenir l'identifiant de la camera à partir de l'url
-const url = new URL(window.location.href) //window.location.href permet d'obtenir l'url de la page actuelle
-const id = url.searchParams.get("id") //"searchParams" permet d'obtenir la valeur de la variable "id"
+/* dans la page "details.html", il faut obtenir l'identifiant de la camera à partir de l'url */
+const url = new URL(window.location.href) 
+/* window.location.href permet d'obtenir l'url de la page actuelle */
+const id = url.searchParams.get("id") 
+/* La propriété "searchParams" permet d'obtenir la valeur de la variable "id". 
+La propriété en lecture seule searchParams de l'interface URL retourne
+un objet URLSearchParams (dans l'exemple qui suit c'est url) permettant d'accéder aux arguments décodés de la requête GET contenu dans l'URL. 
+On peut aussi écrire :
+let url = (new URL(window.location.href)).searchParams;
+let id = url.get("id"); pour la chaine de caractère
+ou let id = parseInt(url.get("id")); pour le nombre
+*/
+
 console.log(id)
 
-//cette fonction permet de stocker les identifiants des cameras ajoutées au panier dans le navigateur (localStorage)
+/* Cette fonction permet de stocker les identifiants des cameras ajoutées au panier dans le navigateur (localStorage). Il faut stocker le id ET la lentille pour avoir un produit unique, sinon si j'ajoute une camera avec une lentille je ne pourrais plus rajouter la même camer avec une lentille différente !!! */
+
+
+ /* construction d'un objet cameraAdded contenat l'id et la lentille de la caméra */
+ let cameraAddedTable = [] /* pas const cameraAddedTable*/ 
+let i=0
 function ajouter(){
-	localStorage.setItem(id, id)
-	// autres syntaxes : localStorage[id] = id; ou encore localStorage.id = id;
+   
+    const lensModel = document.querySelector('#liste').value
+    console.log(lensModel)
+/* localStorage.setItem('bgcolor', document.getElementById('bgcolor').value); */
+    const cameraAdded = {
+           id:id,
+           lentille:lensModel
+       }
+       //console.log(cameraAdded) 
+       //console.log('typeof cameraAdded: ' + typeof cameraAdded)
+       //let cameraAddedTableLength = cameraAddedTable.push(cameraAdded) // cameraAddedTable.push("cameraAdded") retourn un entier !!!!
+      // console.log(cameraAddedTable) 
+      // console.log('typeof cameraAddedTable: ' + typeof cameraAddedTable)
+       //console.log(cameraAdded) 
+       
+       // pbme si je retourne pour ajouter un autre modele avec un id différent, l'ancien ajout est perdu !!!! localStorage.setItem(cameraAddedTable, cameraAddedTable) ou localStorage.setItem(cameraAdded, cameraAdded) n'a pas résolu le pbme !!!! Le pbme a été résolu en utilisant getItem, push et setItem dans cet ordre avec 
+       let cameraAddedTable = JSON.parse(localStorage.getItem(id)) || []; // comme ça ca ne l'écrase pas
+       cameraAddedTable.push(cameraAdded)
+       localStorage.setItem(id, JSON.stringify(cameraAddedTable));
+
+       //localStorage.setItem(JSON.stringify(cameraAddedTable), JSON.stringify(cameraAddedTable))
+    
+       
+       console.log(JSON.stringify(cameraAddedTable)) 
+       console.log(cameraAddedTable)
+      // console.log(localStorage.setItem(cameraAddedTable, JSON.stringify(cameraAddedTable)))
+       //console.log('typeof cameraAddedTable: ' + typeof cameraAddedTable)
+
+
+       //const cameraAddedString = JSON.stringify(cameraAdded)
+       //localStorage.setItem(cameraAddedString, cameraAddedString)
+       //console.log(cameraAddedString) 
+       //console.log('typeof cameraAddedString: ' + typeof cameraAddedString)
+/* autres syntaxes : localStorage[id] = id; ou encore localStorage.id = id; */
 }
 
-//on passe l'identifiant de la camera obtenu vers l'api
-fetch(`http://localhost:3000/api/cameras/${id}`) /*?imageUrl*/
+
+
+/* on passe l'identifiant de la camera obtenu à l'api */
+fetch(`http://localhost:3000/api/cameras/${id}`)
     .then(resp => {
         console.log(resp)
         return resp.json()
     })
     .then(respJson => {
-		console.log(respJson)
-		//l'objet respJson contient toutes les informations de la camera
+        console.log(respJson)
+		/* l'objet respJson contient toutes les informations de la camera. */
         let output = ''
         const elementParent = document.querySelector('#wrapper')
-        output += `<a href="details.html?id=${respJson._id}"><img src="${respJson.imageUrl}" alt="#"
-            class="card-img-top rounded-circle"></a>
+        output += `<img src="${respJson.imageUrl}" alt="#"
+            class="card-img-top rounded-pill p-3">
     <div class="card-body">
         <h5 class="card-title">Nom : ${respJson.name}</h5>
         <h6 class="card-title">Prix : ${respJson.price/100} €</h6>
         <p class="card-text">${respJson.description}</p>
 
-        <select class="form-select" aria-label="Default select example">
-            <option selected>Type de lentilles</option>
-            <option value="2">${respJson.lenses[0]}</option>
-            <option value="2">${respJson.lenses[1]}</option>
+        <select id="liste" class="form-select mb-3" aria-label="Default select example">
+			<option selected>Type de lentilles</option>
+			${respJson.lenses.map(x_lentille => `<option value="${x_lentille}">${x_lentille}</option>`)}
 		</select>
-		<input onclick=ajouter() type="submit" value="Ajouter au panier"/>
-	</div>`
+		<input onclick=ajouter() type="submit" value="Ajouter au panier" class="btn btn-primary"/>
+    </div>`
+   
 		/*for(let i=0; i<respJson.lenses.length; i++){
 			<option value="2">${respJson.lenses[i]}</option>
 		*/
-	
 		/* 
 		<p id="demo" onclick="myFunction()">Click me.</p> ou bien : document.getElementById("demo").onclick = function() {myFunction()}; ou encore : document.getElementById("demo").addEventListener("click", myFunction);
-		*/
-
+        */
+       
         elementParent.innerHTML = output
+       //const lensModel = document.querySelector('#liste').value
+       // console.log(lensModel)  
+     //return lentille
+     
+
     })
