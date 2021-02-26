bcfc2abdb5b53249b92b11e342bb146bf4f0d34d
Massine <massinoueslati@gmail.com156>
2021-02-26 19:44:22 +0100

    Mise en place de chackValidity pour la validation du formulaire

--- panier.js
+++ panier.js
@@ -6,7 +6,6 @@ let values = []
 
 /* Parcourir tous les couples identifiant/lentille stockés dans le navigateur */
 let panierLengthParId = localStorage.length
-console.log(panierLengthParId) // affiche le nombre d'id différents (voir le setItem du localStorage) même si un id contient +ieurs lentilles ca ne compte que 1. Il faut donc aussi boucler sur le nbre de lentilles de chaque id. Car le fetch nous renvoi toutes les infos de l'id concerné.
 
 function getBasket() {
     if (localStorage.getItem("basket") != null) {
@@ -39,7 +38,6 @@ for (let i = 0; i < cart.length; i++) {
             let output = ''
             const elementParent = document.querySelector('#wrapper')
             let elementTotal = document.getElementById('total')
-            console.log(cart[i]);
             output += jsonData.map((camera, x) => { // je mape sur les differents id donc les differentes cameras. si j'ai 3 id donc 3 modeles de cameras differents, alors x =0,1,2.
                 return (`
                     <tr id=${camera._id}>
@@ -65,12 +63,9 @@ for (let i = 0; i < cart.length; i++) {
         })
 }
 
-let boutonSupprime = document.querySelector(".btn-del");
-
 function supprimerArticle(id) {
     let myArticle = document.getElementById(id);
     let elementTotal = document.getElementById('total')
-
     for (let i = 0; i < cart.length; i++) {
         if (jsonData[i]._id === id) {
             jsonData.splice(i, 1)
@@ -79,7 +74,6 @@ function supprimerArticle(id) {
                 sousTotal = jsonData.map((camera, x) => camera.price * cart[x].quantity)
                 total = sousTotal.reduce((acc, curr) => acc + curr)
                 saveBasket(cart)
-                console.log(total);
                 myArticle.remove();
                 elementTotal.innerHTML = total / 100
             } else if (jsonData.length == 0) {
@@ -92,79 +86,78 @@ function supprimerArticle(id) {
 }
 
 /* ************* Envoyer le formulaire de confirmation ************* */
-
-
 let formValid = document.getElementById('myForm');
 formValid.addEventListener('submit', valider);
 
-
 function valider(e) {
     e.preventDefault();
-
     let prenom = document.getElementById('prenom');
-    let missPrenom = document.getElementById('prenom_manquant');
     let firstName = "";
     firstName = document.getElementById('prenom').value;
 
     let nom = document.getElementById('nom');
-    let missNom = document.getElementById('nom_manquant');
     let lastName = "";
     lastName = document.getElementById('nom').value;
 
     let adresse = document.getElementById('adresse');
-    let missAdresse = document.getElementById('adresse_manquant');
     let address = "";
     address = document.getElementById('adresse').value;
 
     let ville = document.getElementById('ville');
-    let missVille = document.getElementById('ville_manquant');
     let city = "";
     city = document.getElementById('ville').value;
 
     let email = document.getElementById('email');
-    let missEmail = document.getElementById('email_manquant');
     let mail = "";
     mail = document.getElementById('email').value;
 
-    let isFormOk = true;
-
     let validPrenom = myValidatorFunction(prenom);
-    console.log(validPrenom);
     let validNom = myValidatorFunction(nom);
     let validAdresse = myValidatorFunction(adresse);
     let validVille = myValidatorFunction(ville);
     let validEmail = myValidatorFunction(email);
 
-    if(!validPrenom){
-        e.preventDefault();
-        isFormOk = false;
-    };
-    if(!validNom){
-        e.preventDefault();
-        isFormOk = false;
-    };
-    if(!validAdresse){
-        e.preventDefault();
-        isFormOk = false;
-    };
-    if(!validVille){
-        e.preventDefault();
-        isFormOk = false;
-    };
-    if(!validEmail){
-        e.preventDefault();
-        isFormOk = false;
-    };
+    prenom.nextSibling.innerHTML = "";
+    nom.nextSibling.innerHTML = "";
+    adresse.nextSibling.innerHTML = "";
+    ville.nextSibling.innerHTML = "";
+    email.nextSibling.innerHTML = "";
 
-    if(isFormOk){
+    let isFormOk = true;
+
+    if (!validPrenom) {
+        e.preventDefault();
+        prenom.nextSibling.innerHTML = prenom.validationMessage;
+        isFormOk = false;
+    };
+    if (!validNom) {
+        e.preventDefault();
+        nom.nextSibling.innerHTML = nom.validationMessage;
+        isFormOk = false;
+    };
+    if (!validAdresse) {
+        e.preventDefault();
+        adresse.nextSibling.innerHTML = adresse.validationMessage;
+        isFormOk = false;
+    };
+    if (!validVille) {
+        e.preventDefault();
+        ville.nextSibling.innerHTML = ville.validationMessage;
+        isFormOk = false;
+    };
+    if (!validEmail) {
+        e.preventDefault();
+        email.nextSibling.innerHTML = email.validationMessage;
+        isFormOk = false;
+    };
+    if (isFormOk) {
         let contact = {
             firstName,
             lastName,
             address,
             city,
-            mail
+            email: mail
         };
-        console.log(contact, products);
         fetch('http://localhost:3000/api/cameras/order', { //l'api doit envoyer l'objet "contact" et la liste "products" vers le serveur
                 method: "POST",
                 headers: {
@@ -181,19 +174,20 @@ function valider(e) {
                 console.log(json);
                 localStorage.setItem("orderId", json.orderId);
                 localStorage.setItem("total", total);
-              //  window.location = "confirmation.html";
+                window.location = "confirmation.html";
             })
             .catch(err => console.log(err));
     };
 }
+
 function myValidatorFunction(elt) {
-    let myValid = elt.validity.patternMismatch;
-console.log(myValid);    
-if (myValid) {
-        //isFormOk = false;
+    let myValid = elt.checkValidity();
+    console.log(myValid);
+    if (!myValid) {
+        isFormOk = false;
         elt.nextSibling.innerHTML = elt.validationMessage;
         return myValid;
-  } else {
-    return myValid;
-  }
-  }
+    } else {
+        return myValid;
+    }
+}
